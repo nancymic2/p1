8b4c91a9c956d3c0ce650c2b084324e18803ef59
Nancy Michell <nancy.ann.michell@hp.com>
2016-02-23 17:01:50 -0500

    new js and image swapping

--- js/images2.js
+++ js/images2.js
@@ -10,19 +10,26 @@ window.onresize=function(){
 
 function sizeit() {
 // onload or on change check the width of the window and get from the data attribute the appropriate image
-	var imgsrc=document.getElementById('serveImg');
+//change to class serveImg and loop through
+	var imgsrc=document.getElementsByClassName('serveImg');
+	for (var i=0; i<imgsrc.length; i++) {
 
-	if (window.matchMedia('(max-width: 640px)').matches) {
-		var smallest=imgsrc.getAttribute('data-small');
-		imgsrc.src=smallest;
-	
-	}
+		if (window.matchMedia('(max-width: 400px)').matches) {
+			var smallest=imgsrc[i].getAttribute('data-small');
+			imgsrc[i].src=smallest;
+		
+		}
 
+		else if (window.matchMedia('(max-width: 640px)').matches) {
+			var med=imgsrc[i].getAttribute('data-med');
+			imgsrc[i].src=med;
+		
+		}
 
-	else {
-		var imglarge=imgsrc.getAttribute('data-large');
-		imgsrc.src=imglarge;
-	}
-
+		else {
+			var imglarge=imgsrc[i].getAttribute('data-large');
+			imgsrc[i].src=imglarge;
+		}
+    }
 }
 
